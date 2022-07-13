5e7304d54c9a9f69626e75c6d9e09a330af2af18
msaddamhk <saddamhk567@gmail.com>
2022-07-13 21:52:58 +0700

    edit jadi mobile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -95,7 +95,11 @@ class UserController extends Controller
 
     public function logout(Request $request)
     {
+
         $token = $request->user()->currentAccessToken()->delete();
         return Apiformatter::success($token, 'Token Revoked');
+
+        // auth()->logout();
+        // return Apiformatter::success();
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,191 @@
 @extends('layout.main')
 
 @section('body')
-    <!-- hero -->
-    <div class="jumbotron jmb1 jumbotron-fluid">
+    {{-- aplikasi --}}
+    {{-- <div class="p-3"> --}}
+    {{-- form --}}
+    <div style="margin-top: 100px;" class="bg-dange d-block d-sm-none p-1 ">
+        <div class="m-4 card"style="border-radius:7px">
+            <div class="card-bod p-3">
+                <form action="{{ route('tampilproduk') }}" method="GET">
+                    <div id="custom-search-input" class="col-md-12">
+                        <div class="input-group ">
+                            <i class="fas fa-search my-auto"></i>
+                            <input class="form-control-borderless col input-lg p-2" placeholder="Silahkan cari Produk "
+                                name="search" type="search" value="{{ request('search') }}" />
+                            <div class="ml-auto">
+                                <button class="btn text-white melayang" style="background-color: #6C5ECF;"
+                                    type="submit">Cari</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- ak form --}}
+    {{-- versi android --}}
+
+    <section class="m-1">
+        <div class="d-block d-sm-none " style="margin-top: -15px;">
+            <div class="jumbotron m-4 containerbaru jumbotron4 ">
+                <div class="bungkus p-4">
+                    <p class="judul7 " data-aos="fade-up" data-aos-delay="100">
+                        Selamat Datang <br> di Resmi Nusa Kreatif
+                    </p>
+                    <p class="judul8" data-aos="fade-up" data-aos-delay="140">
+                        Kepuasan Anda adalah <b>Prioritas Kami</b>
+                    </p>
+                    <div class=" mt-2 " data-aos="fade-up" data-aos-delay="140">
+                        <a class="btn bg1 text-white" href="#produk" role="button">Belanja sekarang</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <style>
+        .jumbotron4 {
+            background-image: url(../foto/ft19.jpg);
+            background-size: cover;
+            position: relative;
+            height: 260px;
+        }
+
+        .jumbotron4::before {
+            content: "";
+            display: block;
+            width: 100%;
+            top: 0;
+            right: 0;
+            left: 0;
+            background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), #f1f1f1);
+            position: absolute;
+            bottom: 0;
+        }
+
+        .containerbaru {
+            position: relative;
+            z-index: 100;
+        }
+
+        .judul7 {
+            font-weight: 500;
+            font-size: 20px;
+            margin-top: 31px
+        }
+
+        .judul8 {
+            font-size: 10px;
+            margin-top: -11px
+        }
+
+        .bg1 {
+            background-color: #6C5ECF;
+            font-size: 7px
+        }
+    </style>
+    </div>
+    {{-- akhir android --}}
+
+
+
+
+
+    {{-- versi tablet --}}
+
+    {{-- form --}}
+    <div style="margin-top: 120px;" class="container d-none d-sm-block d-md-none">
+        <div class="container card"style="border-radius:7px">
+            <div class="card-bod p-3">
+                <form action="{{ route('tampilproduk') }}" method="GET">
+                    <div id="custom-search-input" class="col-md-12">
+                        <div class="input-group ">
+                            <i class="fas fa-search my-auto"></i>
+                            <input class="form-control-borderless col input-lg p-2" placeholder="Silahkan cari Produk "
+                                name="search" type="search" value="{{ request('search') }}" />
+                            <div class="ml-auto">
+                                <button class="btn text-white melayang" style="background-color: #6C5ECF;"
+                                    type="submit">Cari</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- ak form --}}
+
+    <div class="container d-none d-sm-block d-md-none" style="margin-top: 40px">
+        <div class="jumbotron containerbaru jumbotron3">
+            <div class="bungkus my-auto">
+                <h1 class="judul5 " data-aos="fade-up" data-aos-delay="100">
+                    Selamat Datang <br> di Website Resmi Nusa Kreatif
+                </h1>
+                <p class="judul6" data-aos="fade-up" data-aos-delay="140">
+                    Kepuasan Anda adalah <b>Prioritas Kami</b>
+                </p>
+                <div class=" mt-2 " data-aos="fade-up" data-aos-delay="140">
+                    <a class="btn bg
+                text-white" href="#produk" role="button">Belanja
+                        sekarang</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <style>
+        .jumbotron3 {
+            background-image: url(../foto/ft19.jpg);
+            background-size: cover;
+            position: relative;
+            height: 250px;
+            border-radius: 7px;
+        }
+
+        .jumbotron3::before {
+            content: "";
+            display: block;
+            width: 100%;
+            top: 0;
+            right: 0;
+            left: 0;
+            background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), #f1f1f1);
+            position: absolute;
+            bottom: 0;
+            border-radius: 7px;
+        }
+
+        .containerbaru {
+            position: relative;
+            z-index: 100;
+        }
+
+        .judul5 {
+            font-weight: 500;
+            font-size: 25px;
+            margin-top: 11px
+        }
+
+        .judul6 {
+            font-weight: 500;
+            font-size: 12px
+        }
+
+        .bg {
+            background-color: #6C5ECF;
+            font-size: 12px
+        }
+    </style>
+    {{-- akhir vs tablet --}}
+
+
+
+
+
+
+
+    {{-- awal website --}}
+    {{-- hero --}}
+    <div class="jumbotron jmb1 d-none d-md-block jumbotron-fluid">
         <div class="container  containergb" style="  margin-top: 170px;">
             <h1 class="judul" data-aos="fade-up" data-aos-delay="100">
                 Selamat Datang <br> di Website Resmi Nusa Kreatif
@@ -10,10 +193,10 @@
             <p class="judul2" data-aos="fade-up" data-aos-delay="140">
                 Kepuasan Anda adalah <b>Prioritas Kami</b>
             </p>
-
             <div class="center mt-2 " data-aos="fade-up" data-aos-delay="140">
                 <a class="btn btn-primari
-                text-white" href="#produk" role="button">Belanja sekarang</a>
+                text-white" href="#produk" role="button">Belanja
+                    sekarang</a>
             </div>
 
         </div>
@@ -22,7 +205,7 @@
 
 
     <!-- info1 -->
-    <div class="container-fluid">
+    <div class="container-fluid d-none d-md-block mb-5">
         <div class="row justify-content-center">
             <div class="col-10 info1">
                 <div class="row p-1">
@@ -55,15 +238,13 @@
     </div>
     <!-- akhrinfo1 -->
     <!-- kategori -->
-    <section id="kategori" class="kategori-produk  mt-5 p-3">
+    <section id="kategori" class="kategori-produk d-md-block  mt-1 p-3">
         <div class="container">
-            <h5 class="" data-aos="fade-up" style="font-size: 22px; color: #242231; font-weight: 700;">
+            <h5 class="judul10" data-aos="fade-up">
                 Produk Kategori</h5>
             <hr data-aos="fade-up">
-
             <div class="row  mt-4 ">
                 <!-- start row -->
-
                 @php $kategoriaos = 0 @endphp
                 @foreach ($kategoris as $item)
                     {{-- @if ($item->barangs->count() != 0) --}}
@@ -78,20 +259,20 @@
                     </div>
                     {{-- @endif --}}
                 @endforeach
-
                 <!-- end row -->
             </div>
             <a href="/detailkategori" data-aos="fade-up" class="btn text-white mt-3 melayang"
-                style="background-color: #6C5ECF; font-size: 13px"> Lihat
+                style="background-color: #6C5ECF; font-size: 12px"> Lihat
                 Selengkapnya <i class="fas fa-fw fa-arrow-right"></i></a>
         </div>
     </section>
     <!-- ak kategori -->
 
     <!-- card baru -->
-    <section id="produk" class="produk mt-5  p-3 " style="background-color: #f8f8f8; ">
+    <section id="produk" class="produk mt-5 d-md-block p-3 " style="background-color: #f8f8f8; ">
         <div class="container" style="">
-            <h5 data-aos="fade-up" class="mt-4" style="font-size: 22px; color: #242231; font-weight: 700;"> Produk
+            <h5 data-aos="fade-up" class="mt-4 judul10">
+                Produk
                 Unggulan</h5>
             <hr data-aos="fade-up">
             <div class="row mt-4">
@@ -117,37 +298,39 @@
                 <!-- row -->
             </div>
             <a href="/produk" data-aos="fade-up" class="btn text-white mb-4 melayang"
-                style="background-color: #6C5ECF; font-size: 13px">Lihat
+                style="background-color: #6C5ECF; font-size: 12px ">Lihat
                 Selengkapnya <i class="fas fa-fw fa-arrow-right"></i></a>
         </div>
     </section>
     <!-- akhir card baru -->
 
     <!-- yt -->
-    <div id="vidio" class="container mt-4 p-3">
-        <h3 style="color: #242231; font-weight: 700; font-size: 22px;" data-aos="fade-up">
+    <div id="vidio" class="container d-md-block  mt-4 p-4">
+        <h3 class="judul10" data-aos="fade-up">
             Tentang Kami
         </h3>
         <hr data-aos="fade-up">
         <div class="row mt-4">
             <div class="col-md-6">
                 <p style="" class="mb-4" data-aos="fade-up" data-aos-delay="150">
-                    Gampong Nusa adalah sebuah Gampong wisata yang terletak di Kecamatan Lhoknga, Kabupaten Aceh Besar.
-                    Nusa merupakan satu diantara gampong di Aceh yang terus bergerak mengembangkan Desa wisata berbasis
-                    masyarakat. Potensi lokal yang dimiliki terus diramu menjadi berbagai atraksi wisata dengan tujuan
+                    Gampong Nusa adalah sebuah Gampong wisata yang terletak di Kecamatan Lhoknga, Kabupaten Aceh
+                    Besar.
+                    Nusa merupakan satu diantara gampong di Aceh yang terus bergerak mengembangkan Desa wisata
+                    berbasis
+                    masyarakat. Potensi lokal yang dimiliki terus diramu menjadi berbagai atraksi wisata dengan
+                    tujuan
                     utama
                     adalah meningkatkan ekonomi masyarakat dan menjaga keberlanjutan lingkungan.
                     Salah satu yang terkenal dari Gampong ini adalah pengelolaan sampahnya, dari hasil pengelolaan
                     sampah
-                    tersebut akan di daur ulang menjadi barang yang bermanfaat, seperti tas, tempat tisu, dan berbagai
+                    tersebut akan di daur ulang menjadi barang yang bermanfaat, seperti tas, tempat tisu, dan
+                    berbagai
                     macam
                     lainya.
                 </p>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="embed-responsive embed-responsive-21by9 " data-aos="fade-up">
-                    {{-- <iframe class="embed-responsive-item yt" src=" https://www.youtube.com/embed/185FHRdxiTc"></iframe>
-                    https://www.youtube.com/watch?v=185FHRdxiTc --}}
 
                     <iframe class="yt" width="560" height="315" src="https://www.youtube.com/embed/185FHRdxiTc"
                         title="YouTube video player" frameborder="0"
