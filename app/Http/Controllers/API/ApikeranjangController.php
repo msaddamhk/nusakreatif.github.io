63d8a3a39cfd5dc1d200592e4683d42316bf9bec
msaddamhk <saddamhk567@gmail.com>
2022-07-28 00:40:46 +0700

    ubah api 66

--- app/Http/Controllers/API/ApikeranjangController.php
+++ app/Http/Controllers/API/ApikeranjangController.php
@@ -24,7 +24,7 @@ class ApikeranjangController extends Controller
                 return $item;
             });
 
-            dd($keranjang);
+            // dd($keranjang);
             return ApiFormatter::createApi(200, 'Sukses', $keranjang);
         } else {
             return ApiFormatter::createApi(400, 'Gagal');
